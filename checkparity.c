3e51e37bf33acfe6aff6dbc6e01e3028eaaa640c
arj1204 <athulraj1204@gmail.com>
2023-01-13 11:19:28 +0530

    Initial Commit

---
+++ checkparity.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+{
+   int  n,num, flag,count=0;
+   printf("enter the number you need to check : ");
+   scanf("%x",&num);
+   n=num;
+   while(num){
+      if( (num & 1) == 1){
+	 count++;
+      }
+      num=num>>1;
+   }
+   
+   if(count%2==0){
+      printf("the number %d is even parity",n);
+   }
+   else{
+      printf("the number %d is odd parity",n);
+   }
+}
+
